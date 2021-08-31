f58e1634bb533685ead3c84b8396624e127c1b44
AGorczycka <agnieszka.gorczycka95@gmail.com>
2021-08-31 16:31:29 +0200

    feat(swapi-project): Introduce basic layout and styles congifuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardContainerComponent } from './features/card-container/card-container.component';
+import { LayoutComponent } from './features/layout/layout.component';
+import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: CardContainerComponent
+      },
+      {
+        path: '**',
+        component: PageNotFoundComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule { 
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './features/layout/layout.component';
+import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
+import { CardContainerComponent } from './features/card-container/card-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    PageNotFoundComponent,
+    CardContainerComponent
   ],
   imports: [
     BrowserModule,
